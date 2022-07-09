27c8b3ec57c44fe0f590347347f3cf7c9b5942fa
Elfshot <44043197+Elfshot@users.noreply.github.com>
2022-07-09 04:50:08 -0400

    write basic test

---
+++ ttRequests_test.go
@@ -0,0 +1,21 @@
+package tt_requests
+
+import (
+	"testing"
+)
+
+// Tests
+func TestAdd(t *testing.T) {
+	data, err := Get_DataAdv(59504)
+	players, err := Get_Players()
+	fxplayers, err := Get_FxPlayers()
+	sotd, err := Get_Sotd()
+
+	if data == nil || players == nil || fxplayers == nil || sotd == nil {
+		t.Error("One of the requests failed (empty response)")
+	}
+	if err != nil {
+		t.Error(err)
+	}
+
+}
